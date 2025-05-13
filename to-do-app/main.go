0d6abf31386d8f954a62673942556db9ade17872
aatifshaikh007 <aatiftalk3010@gmail.com>
2025-05-13 19:58:47 +0530

    hi

--- to-do-app/main.go
+++ to-do-app/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"time"
+)
 
 func main() {
 	mytodo := Todos{}
@@ -11,9 +15,10 @@ func main() {
 }
 
 type Todo struct {
-	ID        int
-	Title     string
-	Completed bool
+	ID             int
+	Title          string
+	Completed      bool
+	completiontime *time.Time
 }
 type Todos []Todo
 
@@ -31,6 +36,39 @@ func (todos *Todos) lis() {
 		fmt.Printf("ID : %d, title : %s, Completed : %t\n", t.ID, t.Title, t.Completed)
 	}
 }
-func (todos *Todos) toggel(id int) {
 
+func (todos *Todos) validates(index int) error {
+	if index < 0 || index >= len(*todos) {
+		err := errors.New("Invalid index")
+		fmt.Println(err.Error())
+		return err
+	}
+	return nil
+}
+
+func (todos *Todos) toggle(index int) error {
+	if err := todos.validates(index); err != nil {
+		return err
+	}
+
+	todo := (*todos)
+	iscompleted := todo[index].Completed
+	if !iscompleted {
+		completiontime := time.Now()
+		todo[index].completiontime = &completiontime
+	}
+
+	todo[index].Completed = !iscompleted
+	return nil
+
+}
+
+func (todos *Todos) delete(index int) error {
+	if err := todos.validates(index); err != nil {
+		return err
+	}
+
+	todo := *todos
+	*todos = append(todo[:index], todo[index+1:]...)
+	return nil
 }
